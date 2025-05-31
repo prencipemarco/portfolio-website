88a6930e205edd2c9594d60363ed921a3e973767
MarcooooooP <prencipemarco.io@uni.com>
2025-05-31 17:02:52 +0200

    add CV download fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,31 @@
 import { RevealOnScroll } from "./RevealOnScroll.jsx";
 
 export const About = () => {
-  const frontendSkills = [
-    "React",
-    "Vue",
-    "TypeScript",
-    "TailwindCSS",
-    "Svelte",
+  const competenzeSoftware = [
+    "Office",
+    "Excel",
+    "PowerPoint",
+    "Windows",
+    "Linux",
+
+
   ];
 
-  const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
+  const programmingSkills = [
+    "Node.js", 
+    "Python", 
+    "SQL",
+    "JavaScript",
+    "TypeScript",
+    "HTML",
+    "CSS",
+    "React",
+    "C",
+    "C++",
+    "Java",
+    "Git",
+    "GitHub",
+  ];
 
   return (
     <section
@@ -33,7 +49,7 @@ export const About = () => {
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Frontend</h3>
                 <div className="flex flex-wrap gap-2">
-                  {frontendSkills.map((tech, key) => (
+                  {competenzeSoftware.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -49,7 +65,7 @@ export const About = () => {
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Backend</h3>
                 <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((tech, key) => (
+                  {programmingSkills.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
